2458bf09de8f305801ea2b271fc0ce006fd75028
sethjiusa <47671684+sethjiusa@users.noreply.github.com>
2025-04-08 20:52:46 +0000

    Remove obsolete workflow files and implement new API and Dashboard deployment workflows

--- src/ContosoSuitesDashboard/pages/1_Chat_with_Data.py
+++ src/ContosoSuitesDashboard/pages/1_Chat_with_Data.py
@@ -8,6 +8,12 @@ def create_chat_completion(messages):
     """Create and return a new chat completion request. Key assumptions:
     - The Azure OpenAI endpoint and deployment name are stored in Streamlit secrets."""
 
+    search_endpoint = st.secrets["search"]["endpoint"]
+    search_key = st.secrets["search"]["key"]
+    search_index_name = st.secrets["search"]["index_name"]
+
+
+
     # Retrieve secrets from the Streamlit secret store.
     # This is a secure way to store sensitive information that you don't want to expose in your code.
     # Learn more about Streamlit secrets here: https://docs.streamlit.io/develop/concepts/connections/secrets-management
@@ -32,7 +38,22 @@ def create_chat_completion(messages):
             {"role": m["role"], "content": m["content"]}
             for m in messages
         ],
-        stream=True
+        stream=True, 
+        extra_body={
+              "data_sources": [
+                  {
+                      "type": "azure_search",
+                      "parameters": {
+                          "endpoint": search_endpoint,
+                          "index_name": search_index_name,
+                          "authentication": {
+                              "type": "api_key",
+                              "key": search_key
+                          }
+                      }
+                  }
+              ]
+          }
     )
 
 def handle_chat_prompt(prompt):
